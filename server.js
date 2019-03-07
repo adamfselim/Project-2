767159101b9fb4997e74dbee6ee5d2c160261ce6
clagarda <clagarda2390@yahoo.comclagarda2390@yahoo.comgit config --global user.email clagarda2390@yahoo.com>
2019-03-06 19:57:13 -0700

    added filter bar to dashboard

--- server.js
+++ server.js
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
-var express = require("express");
-
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
-
-// Requiring our models for syncing
-var db = require("./models");
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Static directory
-app.use(express.static("public"));
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Routes
-// =============================================================
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
-
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({}).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-=======
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -86,5 +44,4 @@ models.sequelize.sync().then(function () {
 app.listen(PORT, function (err) {
     if (!err)
         console.log("Site is live"); else console.log(err)
->>>>>>> cbf8b593753e572655bf509704c72224bd2e666b
 });
